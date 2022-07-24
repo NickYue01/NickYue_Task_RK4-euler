20d27709f2b95a0520cd27f98784aefbd1697fe5
Nick Yue <iq21822@bristol.ac.uk>
2022-07-25 00:45:47 +0800

    part of the code from odesolve.py is moved into this function, combined with part of the code need to be tested from test_odesolve.py program. The debug will doing much easier with doing this. It is possible to step into the function to know where the problem, while debuging. Also, my program can't directly draw two lines on the same diagram with the code plt.plot_ t,Xt.T_. the array need to be specified. The problem is solved by using plt two times.

---
+++ test_arrays.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 23 11:19:37 2022
+
+@author: nicko
+"""
+
+import numpy as np
+from odesolve import solveto
+# Defines the RHS of our ODE
+def f (X, t ):
+    x , y = X
+    dxdt = y
+    dydt = -x
+    return np.array ( [ dxdt , dydt ] )
+#Initial conditions
+t0 = 0
+x0 = 1
+y0 = 0
+X0 = np.array ( [ x0 , y0 ] )
+h = 0.5
+t1 = 1
+X1 = solveto ( f , X0 , t0 , t1 , h)
+print (X1)
